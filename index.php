3f65acdbdd708f78e004767d85558f24e8b355fa
AndrieTan <60122551+AndrieTan@users.noreply.github.com>
2020-10-24 19:56:30 +0800

    Update index.php

--- index.php
+++ index.php
@@ -9,20 +9,20 @@
             <div class="dropdown">
               <button class="dropbtn">Prelim</button>
               <div class="dropdown-content">
-                <a href="LT2_2\index.html" target="_blank">Learning Task 2.2</a>
-                <a href="LT2_3\index.html" target="_blank">Learning Task 2.3</a>
-                <a href="LT3_2\index.html" target="_blank">Learning Task 3.2</a>
-                <a href="LT3_3\index.html" target="_blank">Learning Task 3.3</a>
-                <a href="LT4_1\index.html" target="_blank">Learning Task 4.1</a>
-                <a href="CLE_1\index.html" target="_blank">Culminating Learning Evidence</a>
+                <a href="LT2_2\index.php" target="_blank">Learning Task 2.2</a>
+                <a href="LT2_3\index.php" target="_blank">Learning Task 2.3</a>
+                <a href="LT3_2\index.php" target="_blank">Learning Task 3.2</a>
+                <a href="LT3_3\index.php" target="_blank">Learning Task 3.3</a>
+                <a href="LT4_1\index.php" target="_blank">Learning Task 4.1</a>
+                <a href="CLE_1\index.php" target="_blank">Culminating Learning Evidence</a>
               </div>
             </div>
             <div class="dropdown">
             <button class="dropbtn">Midterm</button>
               <div class="dropdown-content">
-                <a href="LT5_1\index.html" target="_blank">Learning Task 5.1</a>
-                <a href="LT6_1\index.html" target="_blank">Learning Task 6.1</a>
-                <a href="LT6_2\index.html" target="_blank">Learning Task 6.2</a>
+                <a href="LT5_1\index.php" target="_blank">Learning Task 5.1</a>
+                <a href="LT6_1\index.php" target="_blank">Learning Task 6.1</a>
+                <a href="LT6_2\index.php" target="_blank">Learning Task 6.2</a>
               </div>
             </div>  
         </div>
