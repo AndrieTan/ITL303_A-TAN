017d00fbd3ab015da0bc57a6ab1772c328548b49
AndrieTan <60122551+AndrieTan@users.noreply.github.com>
2020-11-21 21:14:03 +0800

    Add files via upload

---
+++ CLE_2/index.php
@@ -0,0 +1,41 @@
+<html>
+    <head>
+        <title>FLOOM</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css"> 
+    </head>
+    <body>
+            <object type="image/svg+xml" data="img/Background_SVG.svg" class="bg"></object>
+        <div class = "wrapper">
+            <object type="image/svg+xml" data="img/Tulip_Yellow_SVG.svg" id="yellow_1"></object>
+            <object type="image/svg+xml" data="img/Tulip_Yellow_SVG.svg" id="yellow_2"></object>
+            <object type="image/svg+xml" data="img/Tulip_Yellow_SVG.svg" id="yellow_3"></object>
+            <object type="image/svg+xml" data="img/Tulip_Yellow_SVG.svg" id="yellow_4"></object>
+            <object type="image/svg+xml" data="img/Tulip_Yellow_SVG.svg" id="yellow_5"></object>
+            <object type="image/svg+xml" data="img/Tulip_Yellow_SVG.svg" id="yellow_6"></object>
+            <object type="image/svg+xml" data="img/Tulip_Yellow_SVG.svg" id="yellow_7"></object>
+            <object type="image/svg+xml" data="img/Flower_Pink_SVG.svg" id="pink_1"></object>
+            <object type="image/svg+xml" data="img/Flower_Pink_SVG.svg" id="pink_2"></object>
+            <object type="image/svg+xml" data="img/Flower_Pink_SVG.svg" id="pink_3"></object>
+            <object type="image/svg+xml" data="img/Flower_Pink_SVG.svg" id="pink_4"></object>
+            <object type="image/svg+xml" data="img/Flower_Pink_SVG.svg" id="pink_5"></object>
+            <object type="image/svg+xml" data="img/Flower_Pink_SVG.svg" id="pink_6"></object>
+            <object type="image/svg+xml" data="img/Flower_Pink_SVG.svg" id="pink_7"></object>
+            <object type="image/svg+xml" data="img/Tulip_Red_SVG.svg" id="red_1"></object>
+            <object type="image/svg+xml" data="img/Tulip_Red_SVG.svg" id="red_2"></object>
+            <object type="image/svg+xml" data="img/Tulip_Red_SVG.svg" id="red_3"></object>
+            <object type="image/svg+xml" data="img/Tulip_Red_SVG.svg" id="red_4"></object>
+            <object type="image/svg+xml" data="img/Tulip_Red_SVG.svg" id="red_5"></object>
+            <object type="image/svg+xml" data="img/Tulip_Red_SVG.svg" id="red_6"></object>
+            <object type="image/svg+xml" data="img/Tulip_Red_SVG.svg" id="red_7"></object>
+            <object type="image/svg+xml" data="img/Flower_White_SVG.svg" id="white_1"></object>
+            <object type="image/svg+xml" data="img/Flower_White_SVG.svg" id="white_2"></object>
+            <object type="image/svg+xml" data="img/Flower_White_SVG.svg" id="white_3"></object>
+            <object type="image/svg+xml" data="img/Flower_White_SVG.svg" id="white_4"></object>
+            <object type="image/svg+xml" data="img/Flower_White_SVG.svg" id="white_5"></object>
+            <object type="image/svg+xml" data="img/Flower_White_SVG.svg" id="white_6"></object>
+            <object type="image/svg+xml" data="img/Flower_White_SVG.svg" id="white_7"></object>
+            <object type="image/svg+xml" data="img/Floom_SVG.svg" id="floom"></object>
+        </div>
+
+    </body>
+</html>
