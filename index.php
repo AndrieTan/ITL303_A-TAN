3abe58099bf4ffa302d9ad6a2980c76bfc7e9391
AndrieTan <60122551+AndrieTan@users.noreply.github.com>
2020-12-19 17:55:47 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Andrie's Portfolio</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+
+
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Roboto:100,300,400,500,700|Philosopher:400,400i,700,700i" rel="stylesheet">
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="lib/animate/animate.min.css" rel="stylesheet">
+
+  <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+  <header id="header" class="header header-hide">
+    <div class="container">
+      <nav id="nav-menu-container">
+        <ul class="nav-menu">
+          <li class="menu-active"><a href="#hero">Home</a></li>
+          <li><a href="#features">About</a></li>
+          <li class="menu-has-children"><a href="">Prelim</a>
+            <ul>
+              <li><a href="https://itl303-a-tan.herokuapp.com/LT2_2/index.php" target="_blank">Learning Task 2.2</a></li>
+              <li><a href="https://itl303-a-tan.herokuapp.com/LT2_3/index.php" target="_blank">Learning Task 2.3</a></li>
+              <li><a href="https://itl303-a-tan.herokuapp.com/LT3_2/index.php" target="_blank">learning Task 3.2</a></li>
+              <li><a href="https://itl303-a-tan.herokuapp.com/LT3_3/index.php" target="_blank">Learning Task 3.3</a></li>
+              <li><a href="https://itl303-a-tan.herokuapp.com/LT4_1/index.php" target="_blank">Learning Task 4.1</a></li>
+              <li><a href="https://itl303-a-tan.herokuapp.com/CLE_1/index.php" target="_blank">Culminating Learning Evidence (Prelim)</a></li>
+            </ul>
+          </li>
+          <li class="menu-has-children"><a href="">Midterm</a>
+            <ul>
+              <li><a href="https://itl303-a-tan.herokuapp.com/LT5_1/index.php" target="_blank">Learning Task 5.1</a></li>
+              <li><a href="https://itl303-a-tan.herokuapp.com/LT6_1/index.php" target="_blank">Learning Task 6.1</a></li>
+              <li><a href="https://itl303-a-tan.herokuapp.com/LT6_2/index.php" target="_blank">Learning Task 6.2</a></li>
+              <li><a href="https://itl303-a-tan.herokuapp.com/CLE_2/index.php" target="_blank">Culminating Learning Evidence (Midterm)</a></li>
+            </ul>
+          </li>
+          <li class="menu-has-children"><a href="">Endterm</a>
+            <ul>
+              <li><a href="https://itl303-a-tan.herokuapp.com/LT7_2/index.php" target="_blank">Learning Task 7.2</a></li>
+            </ul>
+          </li>
+          <li><a href="#contact">Contact</a></li>
+          <li><a href="https://github.com/AndrieTan">Github</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+
+  <section id="hero" class="wow fadeIn">
+    <div class="hero-container">
+      <h1>Andrie's Portfolio </h1>
+      <h2>CSS, SVG, JS, &amp; HTML5 Canvas</h2>
+    </div>
+  </section>
+
+  <section id="features" class="padd-section text-center wow fadeInUp">
+
+    <div class="container">
+      <div class="section-title text-center">
+        <h2>Website Design</h2>
+        <p class="separator">I'm experienced with different website design techniques.</p>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+
+        <div class="col-md-6 col-lg-3">
+          <div class="feature-block">
+            <img src="img/svg/code.svg" alt="img" class="img-fluid">
+            <h4>Cascading Style Sheets</h4>
+            <p>Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language such as HTML. 
+              Having to learn CSS enables me to design my website neatly.
+            </p>
+          </div>
+        </div>
+
+        <div class="col-md-6 col-lg-3">
+          <div class="feature-block">
+            <img src="img/svg/vector.svg" alt="img" class="img-fluid">
+            <h4>Scalable Vector Graphics</h4>
+            <p>Scalable Vector Graphics is an Extensible Markup Language-based vector image format for two-dimensional graphics with support for interactivity and animation.
+              Using SVGs makes me create different artworks by only utizlizing code.
+            </p>
+          </div>
+        </div>
+
+        <div class="col-md-6 col-lg-3">
+          <div class="feature-block">
+            <img src="img/svg/design-tool.svg" alt="img" class="img-fluid">
+            <h4>Javascript</h4>
+            <p>JavaScript, often abbreviated as JS, is a programming language that conforms to the ECMAScript specification. JavaScript is high-level, often just-in-time compiled, and multi-paradigm. It has curly-bracket syntax, dynamic typing, prototype-based object-orientation, and first-class functions.
+              JS gave me different options on how to execute different designs and functions for my websites.
+            </p>
+          </div>
+        </div>
+
+        <div class="col-md-6 col-lg-3">
+          <div class="feature-block">
+            <img src="img/svg/pixel.svg" alt="img" class="img-fluid">
+            <h4>HTML5 Canvas</h4>
+            <p>The canvas element is part of HTML5 and allows for dynamic, scriptable rendering of 2D shapes and bitmap images. It is a low level, procedural model that updates a bitmap and does not have a built-in scene graph.
+              Even though it is inferior to SVG, it is useful if I'm making 2d graphics and convert it to 3d.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <hr width="50%">
+  <section id="contact" class="padd-section wow fadeInUp">
+
+    <div class="container">
+      <div class="section-title text-center">
+        <h2>Contact</h2>
+        <p class="separator">Have any questions? Feel free to contact me!</p>
+      </div>
+    </div>
+    <br>
+    <br>
+    <div class="container">
+      <div class="row justify-content-center">
+
+        <div class="col-lg-3 col-md-4">
+
+          <div class="info">
+            <div>
+              <i class="fa fa-map-marker"></i>
+              <p>Bacolod City<br>Philippines</p>
+            </div>
+            <div class="email">
+              <i class="fa fa-envelope"></i>
+              <p>s1820422@usls.edu.ph</p>
+            </div>
+              <br>
+            <div>
+              <i class="fa fa-phone"></i>
+              <p>+63912345678</p>
+            </div>
+          </div>
+        </div>
+  <footer class="footer">
+    <div class="container">
+      <div class="row">
+
+        <div class="col-md-12 col-lg-4">
+          <div class="footer-logo">
+
+            <a class="navbar-brand" href="#">Social Media</a>
+            <p>Follow my social medias to learn more about me and my daily life!</p>
+
+          </div>
+        </div>
+
+        <div class="col-sm-6 col-md-3 col-lg-2">
+          <div class="list-menu">
+            <div class="social-links">
+              <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+              <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
+              <a href="#" class="instagram"><i class="fa fa-instagram"></i></a>
+              <a href="#" class="google-plus"><i class="fa fa-google-plus"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+
+  <script src="lib/jquery/jquery.min.js"></script>
+  <script src="lib/jquery/jquery-migrate.min.js"></script>
+  <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="lib/superfish/hoverIntent.js"></script>
+  <script src="lib/superfish/superfish.min.js"></script>
+  <script src="lib/easing/easing.min.js"></script>
+  <script src="lib/modal-video/js/modal-video.js"></script>
+  <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+  <script src="lib/wow/wow.min.js"></script>
+  <script src="js/main.js"></script>
+
+</body>
+</html>
